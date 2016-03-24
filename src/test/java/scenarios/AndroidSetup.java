73f8578cda456643b53198adf293d074014376c4
Tegar <tegar.raharditya@olx.co.id>
2016-03-24 14:15:53 +0700

    Adding Page Object for Appium mobiile testing. Some simple wrapping Connect to Genymotion

---
+++ src/test/java/scenarios/AndroidSetup.java
@@ -0,0 +1,39 @@
+package scenarios;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class AndroidSetup {
+    protected AndroidDriver driver;
+
+    protected void prepareAndroidForAppium() throws MalformedURLException {
+       // File appDir = new File("/Users/tegar/AppiumDemo/apps");
+       // File app = new File(appDir, "app-olxid-release.6.1.3.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("device","Android");
+
+        capabilities.setCapability("appPackage", "com.app.tokobagus.betterb");
+        capabilities.setCapability("appActivity", "sea.olx.activities.SplashscreenActivity");
+        
+        //mandatory capabilities
+        capabilities.setCapability("deviceName","Android");
+        capabilities.setCapability("platformName","Android");
+        
+        //No Reset Apps
+        capabilities.setCapability("no-reset", "true");
+        capabilities.setCapability("full-reset", "False");
+        capabilities.setCapability("noReset", true);
+        capabilities.setCapability("fullReset", "false");
+        
+        
+        //other caps
+        //capabilities.setCapability("app", app.getAbsolutePath());
+        driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
