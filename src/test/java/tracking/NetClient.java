2ddd0983bf221084cd85caa026ea26928cf1f715
Buddy Arifin <buddy.arifin@olx.co.id>
2016-06-23 17:37:33 +0700

    Add conditions for success test for the first time

--- src/test/java/tracking/NetClient.java
+++ src/test/java/tracking/NetClient.java
@@ -237,10 +237,21 @@ public class NetClient extends BasePage {
             System.out.println("Update Existing Bugs for id : "+ checkId);
             updateCase(testResult, checkId);
         } else if ( checkId <= 0 ) {
-            System.out.println("Creating new bugs ...");
-            create(testResult);
+            createBugsCase(testResult);
         } else {
             System.out.println("Not integrate anything ");
+        }
+    }
+
+    public void createBugsCase(ITestResult result) {
+        switch (result.getStatus()) {
+            case 1 : System.out.println("Test Succeded");
+                     break;
+            case 2 : System.out.println("Creating new bugs ...");
+                     create(result);
+                     break;
+            default:
+                System.out.println("Invalid Status from ITestResult - Created Case");
         }
     }
 
@@ -259,7 +270,7 @@ public class NetClient extends BasePage {
             case 4 : System.out.println("Test Read as Success precentage failure");
                      break;
             default:
-                System.out.println("Invalid Status from ITestResult");
+                System.out.println("Invalid Status from ITestResult - Updated Case");
         }
 
     }
